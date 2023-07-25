125e486c6228f12104ea827239e351f46b375de6
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-25 23:16:01 +0300

    Run count to 1 tes

--- src/main/java/tk/itish/PingPongTest.java
+++ src/main/java/tk/itish/PingPongTest.java
@@ -1,4 +1,13 @@
 package tk.itish;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // define a class
 public class PingPongTest {
+    public List<Object> runPingPong(int countUpTo) {
+        List<Object> results = new ArrayList<>();
+        results.add(1);
+        return results;
+    }
 }
--- src/test/java/tk/itish/PingPongTestTest.java
+++ src/test/java/tk/itish/PingPongTestTest.java
@@ -1,4 +1,22 @@
+package tk.itish;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PingPongTestTest {
-  
+    // test the first count
+    @Test
+    void runPingPong_countUptoOne_ArrayList(){
+        PingPongTest pingPongTest = new PingPongTest();
+        List<Object> expectedOutPut = new ArrayList<>();
+        expectedOutPut.add(1);
+        assertEquals(expectedOutPut, pingPongTest.runPingPong(1));
+    }
 }
