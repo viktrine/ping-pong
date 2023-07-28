693ed66dd36948fdca24101a7ac6b31f32f9252f
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-28 21:24:09 +0300

    Add seconda date format

--- src/main/java/tk/itish/DateTest.java
+++ src/main/java/tk/itish/DateTest.java
@@ -13,9 +13,13 @@ public class DateTest {
 
         System.out.println();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y/M/dd");
-        System.out.println("Formatted Date: " + simpleDateFormat.format(date));
+        final String printOutString = "Formatted Date: " + simpleDateFormat.format(date);
+        System.out.println(printOutString);
 
-        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd/MM/YYYY");
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println("Formatted Date: " + simpleDateFormat2.format(date));
+
+        SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println("Formatted Date: " + simpleDateFormat3.format(date));
     }
 }
