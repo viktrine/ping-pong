51076e1a65311d67f6747e212d1d2ea54c3ee121
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-28 20:30:15 +0300

    Add seconda date format

---
+++ src/main/java/tk/itish/DateTest.java
@@ -0,0 +1,21 @@
+package tk.itish;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// test date items
+public class DateTest {
+    public static void main(String[] args) {
+        System.out.println("Print Dates");
+
+        Date date = new Date();
+        System.out.println("unformatted Date: "+date);
+
+        System.out.println();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y/M/dd");
+        System.out.println("Formatted Date: " + simpleDateFormat.format(date));
+
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd/MM/YYYY");
+        System.out.println("Formatted Date: " + simpleDateFormat.format(date));
+    }
+}
