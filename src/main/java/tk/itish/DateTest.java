ba944606181c7c647d9e17c462c622cd2933106f
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-29 15:25:19 +0300

    Add extra lines

--- src/main/java/tk/itish/DateTest.java
+++ src/main/java/tk/itish/DateTest.java
@@ -15,8 +15,6 @@ public class DateTest {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
-        System.out.println();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y/M/dd");
         final String printOutString = "Formatted Date: " + simpleDateFormat.format(date);
